6db23cb8838f63a7bfde2f76a3674dfaf3b48f22
korietsu <spht.lhar@gmail.com>
2014-12-14 21:29:02 -0600

    Fix Last Ghrah Animation Sub
    Uses OR instead of logical AND

--- scripts/globals/mobskills/Core_Meltdown.lua
+++ scripts/globals/mobskills/Core_Meltdown.lua
@@ -10,7 +10,7 @@ require("/scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function OnMobSkillCheck(target,mob,skill)
-    if(mob:isMobType(MOBTYPE_NOTORIOUS) and mob:AnimationSub ~= 0) then
+    if(mob:isMobType(MOBTYPE_NOTORIOUS) or mob:AnimationSub ~= 0) then
         return 1;
     end
     return 0;
